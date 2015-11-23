cb577ac975eaf19635863ca164d7e2104d6e17cc
Christopher R. Palmer <crpalmer@gmail.com>
2015-11-23 04:33:59 -0500

    wsbm: Add support for LP blobs that predate ION support
    Change-Id: I6e991a3f1094d7a4f82aa32b005374633abdc279

--- src/wsbm_ttmpool.c
+++ src/wsbm_ttmpool.c
@@ -556,7 +556,9 @@ ttm_pool_ub_create(struct _WsbmBufferPool *pool, unsigned long size, uint32_t pl
     arg.req.placement = placement;
     arg.req.page_alignment = alignment / pageSize;
     arg.req.user_address = (unsigned long)user_ptr;
+#ifndef HAVE_LP_BLOBS
     arg.req.fd = fd;
+#endif
 
     DRMRESTARTCOMMANDWRITEREAD(pool->fd, ttmPool->devOffset + TTM_PL_CREATE_UB,
 			       arg, ret);
